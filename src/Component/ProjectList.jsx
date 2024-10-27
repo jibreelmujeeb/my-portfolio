ba7722b87313c1c395a902404821be9c2bdb1926
jibreel mujeeb <jibreelmujeeb@gmail.com>
2024-10-27 22:13:35 +0100

    commit

--- src/Component/ProjectList.jsx
+++ src/Component/ProjectList.jsx
@@ -1,14 +1,14 @@
 import img from "../../src/assets/printivo.png"
 import img1 from "../../src/assets/travel.jpeg"
-import img2 from "../../src/assets/air line.jpg"
+import img2 from "../../src/assets/budget.png"
 import img3 from "../../src/assets/love.jpeg"
-import img4 from "../../src/assets/asset1.jpg"
-import img5 from "../../src/assets/funghi.jpg"
+import img4 from "../../src/assets/wether.png"
+import img5 from "../../src/assets/coffee.jpg"
 export const ProjectList = [
   {
     image: img,
     title: "Simple E-Commerce App",
-    gitlink: "https://printvol.vercel.app/",
+    gitlink: "https://budgetapp-rho.vercel.app/",
     content:
       "Discover, shop, and simplify your online purchases with the Simple E-Commerce App. Browse and buy your favorite products effortlessly from your smartphone. It's e-commerce made easy.",
   },
@@ -21,30 +21,30 @@ export const ProjectList = [
   },
   {
     image: img2,
-    title: "Air line ",
-    gitlink: "https://olayiwola2288.github.io/Ola-s-airway/",
+    title: "Budget App",
+    gitlink: "https://budget-app-2.vercel.app/",
     content:
-      "Airline Booking App: Effortlessly book your flights. Find, compare, and reserve your dream destinations with ease. It's your travel companion, simplifying your journey with a tap.",
+      "The budget app is designed to help users manage their finances effectively by tracking income, expenses, and savings. It aims to provide users with insights into their spending habits and assist them in achieving their financial goals.",
   },
   {
     image: img3,
     title: "Love calculator App",
-    gitlink: " https://love-cal-three.vercel.app/",
+    gitlink: " https://love-calculator-ten-iota.vercel.app/",
     content:
       "Love Calculator App: Discover your love compatibility in seconds. Input your names, and let the app work its magic. Share the results with friends or enjoy a lighthearted look at your own connections. Love made simple.",
   },
   {
     image: img4,
-    title: "TAXI App",
-    gitlink: " https://olayiwola2288.github.io/Go-TAXY/",
+    title: "Wether App",
+    gitlink: " https://weather-app-alpha-five-41.vercel.app/",
     content:
-      "Taxi App: Your on-demand ride solution. Just a few taps on your smartphone, and you're on your way. Whether it's a trip to the airport, a night out, or an important meeting, our user-friendly app ensures quick and reliable transportation. Track your driver, make cashless payments, and enjoy top-notch service. Say goodbye to waiting on street corners – the Taxi App is your trusted ride companion.",
+      "Wether App:Stay ahead of the weather with our app! Get real-time updates, hourly forecasts, and severe weather alerts right at your fingertips. Whether you’re planning a picnic or just want to know if you need an umbrella, we’ve got you covered!",
   },
   {
     image: img5,
-    title: "Pizza Menu App",
-    gitlink: "https://salaudeen-fastpizza.vercel.app/",
+    title: "Coffee Klang",
+    gitlink: "https://jibreel.vercel.app/",
     content:
-      "Pizza Menu App: Your one-stop slice of pizza heaven. Explore a delectable array of pizza options, customize your perfect pie, and order for dine-in or delivery in a snap. For pizza lovers, by pizza lovers.",
+      "At Coffee Klang, we believe in serving more than just coffee; we serve experiences. Whether you’re here for a quick pick-me-up or to unwind with friends, our cozy ambiance and delicious offerings are sure to delight.",
   },
 ];
